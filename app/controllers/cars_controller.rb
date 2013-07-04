ece2c98b2a6549f0059d9e7197c01adfb03e8d84
bj <britneyjo.ludkowski@gmail.com>
2013-07-04 01:02:26 +0100

    CRUD abilities

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,6 +4,41 @@ def index
   @cars = Car.all
 end
 
+def show
+  @car = Car.find(params[:id])
+end
+
+def new
+  @car = Car.new
+end
+
+def create
+  @car = Car.new(params[:car])
+  @car.save
+  redirect_to @car
+end
+
+def edit
+  @car = Car.find(params[:id])
+end
+
+def update
+  @car = Car.find(params[:id])
+  @car.update_attributes(params[:car])
+  redirect_to @car
+end
+
+def destroy
+  @car = Car.find(params[:id])
+  @car.destroy
+  redirect_to cars_path
+end
+
+  # case image
+  #   when @car = Car.
+  #   image_tag("/icons/icon.gif", height: '32', width: '32')
+  # end
+
 
 end
 
